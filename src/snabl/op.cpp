d3eca5f5c4eb6ddf89df59e13b26e0743e5ba50f
codr4life <codr4life@gmail.com>
2018-08-16 10:18:07 +0200

    fixed op type

--- src/snabl/op.cpp
+++ src/snabl/op.cpp
@@ -1,13 +1,18 @@
 #include "snabl/op.hpp"
 
-namespace snabl {	
+namespace snabl {
+	OpType::OpType(const std::string &id, size_t label_offs):
+		id(id), label_offs(label_offs) {
+	}
+
 	Op::Op(const OpType &type, const std::any &data):
 		type(type), _data(data) {
 	}
 
 	namespace ops {
-		static const OpType Push::type("Push", 0);
+		const OpType Push::type("Push", 0);
 
-		Push::Push(const Box &value): value(value);
+		Push::Push(const Box &value): value(value) {
+		}
 	}
 }
