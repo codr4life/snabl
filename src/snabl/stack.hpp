b46579512ab4c9d09aaaf00d1994fa55684d37d1
codr4life <codr4life@gmail.com>
2018-08-25 17:45:11 +0200

    switched stack type

--- src/snabl/stack.hpp
+++ src/snabl/stack.hpp
@@ -1,11 +1,12 @@
 #ifndef SNABL_STACK_HPP
 #define SNABL_STACK_HPP
 
-#include <deque>
+#include <vector>
+
 #include "snabl/box.hpp"
 
 namespace snabl {
-	using Stack = std::deque<Box>;
+	using Stack = std::vector<Box>;
 }
 
 #endif
