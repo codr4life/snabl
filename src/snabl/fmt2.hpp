27a2bb3e572749b4c9ae753b86fe1c7e110563cd
codr4life <codr4life@gmail.com>
2018-08-28 15:52:03 +0200

    renamed type arg

--- src/snabl/fmt2.hpp
+++ src/snabl/fmt2.hpp
@@ -9,17 +9,17 @@ namespace snabl {
 	string fmt2_arg(const char* x);
 	string fmt2_arg(const string &x);
 
-	template <typename ArgT>
-	string fmt2_arg(const ArgT &x) { return to_string(x); }
+	template <typename T>
+	string fmt2_arg(const T &x) { return to_string(x); }
 
 	struct fmt2_conv {
-    template<class T>
+    template<typename T>
     fmt2_conv(T&& val);
 
 		const string as_str;
 	};
 
-	template<class T>
+	template<typename T>
 	fmt2_conv::fmt2_conv(T&& val): as_str(fmt2_arg(val)) { }
 	
 	string fmt2(string_view spec, initializer_list<fmt2_conv> args);
