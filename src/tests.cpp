351a3d266aca98397b0be503f54d00f1f085002c
codr4life <codr4life@gmail.com>
2018-09-06 17:40:22 +0200

    converted tests

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,11 +1,7 @@
 #include <cassert>
 
-#include "snabl/call.hpp"
-#include "snabl/env.hpp"
 #include "snabl/fmt.hpp"
-#include "snabl/func.hpp"
 #include "snabl/ptr.hpp"
-#include "snabl/type.hpp"
 
 namespace snabl {
 	void fmt_tests() {
@@ -13,61 +9,6 @@ namespace snabl {
 		assert(fmt("%0 %%1 %1", {"foo", "bar"}) == "foo %1 bar");
 		assert(fmt("%0 %0", {""}) == " ");
 		assert(fmt("%%0", {}) == "%0");
-	}
-
-	void lang_tests() {
-		using namespace snabl;
-
-		Env env;
-
-		env.run("1 +, 3 * 2");
-		assert(env.pop().as<Int>() == Int(7));
-
-		env.run("1 + (3 * 2)");
-		assert(env.pop().as<Int>() == Int(7));
-
-		env.run("(1 +, 5 - 2)");
-		assert(env.pop().as<Int>() == Int(4));
-
-		env.run("1 3 5 drop +");
-		assert(env.pop().as<Int>() == Int(4));
-
-		env.run("21 dup +");
-		assert(env.pop().as<Int>() == Int(42));
-
-		env.run("1 3 swap -");
-		assert(env.pop().as<Int>() == Int(2));
-
-		env.run("1 2 3 rot -; -");
-		assert(env.pop().as<Int>() == Int(4));
-
-		env.run("(let: foo 42) @foo");
-		assert(env.pop().as<Int>() == Int(42));
-
-		env.run("{@foo} call");
-		assert(env.pop().as<Int>() == Int(42));
-		
-		env.run("1 +<_ Int> 3");
-		assert(env.pop().as<Int>() == Int(4));
-
-		env.run("if: (1 < 3) 5 7");
-		assert(env.pop().as<Int>() == Int(5));
-
-		env.run("if: (3 < 1) 5 7");
-		assert(env.pop().as<Int>() == Int(7));
-		
-		env.run("func: foo1<A> A (* 2) 21 foo1");
-		assert(env.pop().as<Int>() == Int(42));
-		
-		env.run("func: fib<Int> Int,\n"
-						"  let: n _\n"
-						"  if: (@n < 2) @n, (fib, @n - 1) + (fib, @n - 2)");
-		
-		env.run("fib 10");
-		assert(env.pop().as<Int>() == Int(55));
-
-		env.run("ms, bench 3 {sleep, 10ms}");
-		assert(env.pop().as<Int>() > Int(15));
 	}
 
 	void ptr_ref_tests() {
@@ -116,7 +57,6 @@ namespace snabl {
 	
 	void all_tests() {
 		fmt_tests();
-		lang_tests();
 		ptr_tests();
 	}
 }
---
+++ tests/all.sl
@@ -0,0 +1,37 @@
+(test=, nil bool; f)
+
+(test=, t bool; t)
+(test=, f bool; f)
+
+(test=, 0 bool; f)
+(test=, 1 bool; t)
+
+(test=, 1 = 1; t)
+(test=, 1 = 3; f)
+
+(test=, 1 < 3; t)
+(test=, 3 < 1; f)
+
+(test=, 3.14 int; 3)
+
+(test=, 1 + 3; 4)
+(test= (1 +, 3 * 2) 7)
+(test= (1 + (3 * 2)) 7)
+(test= (1 +, 5 - 2) 4)
+
+(test=, 1 3 5 drop +; 4)
+(test=, 21 dup +; 42)
+(test=, 1 3 swap -; 2)
+(test= (1 2 3 rot -; -) 4)
+
+(let: foo 42)
+(test=  @foo 42)
+(test=, {@foo} call 42)
+
+(test=, 1 +<_ Int> 3; 4)
+
+(test= if: (1 < 3) 5 7 5)
+(test= if: (3 < 1) 5 7 7)
+
+func: foo1<A> A (* 2)
+(test=, 21 foo1; 42)
