d141ad57b181fbd9b9a4e6292e4dbc359c07846b
codr4life <codr4life@gmail.com>
2018-09-10 15:36:22 +0200

    added free list

--- src/snabl/alloc.hpp
+++ src/snabl/alloc.hpp
@@ -1,12 +1,19 @@
 #ifndef SNABL_ALLOC_H
 #define SNABL_ALLOC_H
 
+#include <cassert>
 #include "snabl/std.hpp"
 
 namespace snabl {
-	template <typename T>
+	template <typename T, size_t NMAX=32>
 	class Alloc: public allocator<T> {
-	public:			
+	public:
+		struct Slot {
+			Slot *next;
+			unsigned char val[sizeof(T)];			
+			Slot(): next(nullptr) { }
+		};
+			
 		using size_type = size_t;
 		using value_type = T;
 		using pointer = T*;
@@ -15,21 +22,46 @@ namespace snabl {
 		template <typename U>
 		struct rebind { typedef Alloc<U> other; };
 
-		Alloc() noexcept: allocator<T>() { }
-		Alloc(const Alloc<T>& other) noexcept: allocator<T>(other) { }
+		Alloc() noexcept: allocator<T>(), _free() {}
+		
+		Alloc(const Alloc<T>& other) noexcept: allocator<T>(other), _free() { }
 
 		template<class U>
-		Alloc(const Alloc<U>& other) noexcept: allocator<T>(other) { }
+		Alloc(const Alloc<U>& other) noexcept: allocator<T>(other), _free() { }
+
+		~Alloc() {
+			for (size_t i(0); i < NMAX; i++) {
+				auto s(_free[i]);
+				if (i) { delete[] s; } else { delete s; }
+			}
+		}
 		
 		T* allocate(size_type n, const void *hint=0) {
+			assert(n < NMAX);
 			//cout << "allocate " << n*sizeof(T) << endl;
-			return allocator<T>::allocate(n, hint);
+			Slot *s(_free[n-1]);
+
+			if (s) {
+				_free[n-1] = s->next;
+			} else {
+				s = (n == 1) ? new Slot : new Slot[n];
+			}
+			
+			return reinterpret_cast<T *>(&s->val);
 		}
 
 		void deallocate(T *p, size_type n) {
+			assert(n < NMAX);
 			//cout << "deallocate " << n*sizeof(T) << endl;
-			allocator<T>::deallocate(p, n);
+			Slot *s(reinterpret_cast<Slot *>(reinterpret_cast<unsigned char *>(p)-
+																			 offsetof(Slot, val)));
+
+			s->next = _free[n-1];
+			_free[n-1] = s;			
 		}
+
+	private:
+		array<Slot *, NMAX> _free;
 	};
 }
 
