c7e8f2fd996cdb7d1c54e47b8111d818266b6ec9
codr4life <codr4life@gmail.com>
2018-09-11 21:59:38 +0200

    moved fmts

--- src/snabl/fmt1.cpp
+++
@@ -1,9 +0,0 @@
-#include "snabl/fmt1.hpp"
-
-namespace snabl {
-	string fmt1_arg(const char *x) { return string(x); }
-	
-	string fmt1_arg(const string &x) { return x; }
-
-	void _fmt1(string &spec, size_t i) { }
-}
